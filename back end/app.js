d2f9412c70fa5059d60496ca965934f4b4c6e745
Jessica D <deck.jessica@gmail.com>
2021-01-12 20:54:49 -0500

    tried to get html files to match to add new tables

---
+++ back end/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// tables and reservations (DATA)
+// =============================================================
+var availTables = [];
+
+var reserveTables = [];
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
