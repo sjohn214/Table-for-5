b67f4158cb4894ee40c77ef71d997fd805432fca
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-01-12 19:55:00 -0500

    Add dependencies

---
+++ back end/app.js
@@ -0,0 +1,14 @@
+// Dependencies
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 8080;
+
+// Data
+
+var makeReservation = {
+    name: "",
+
+}
