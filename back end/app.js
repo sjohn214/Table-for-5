a7f81219524eaf6040bf52622265b66651735ff1
Shannon <shannon.page1218@outlook.com>
2021-01-12 20:18:06 -0500

    Add routes

--- back end/app.js
+++ back end/app.js
@@ -8,7 +8,53 @@ var PORT = 8080;
 
 // Data
 
-var makeReservation = {
-    name: "",
+var tables = [
+    {
+        routeName: 'Table1',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
 
-}
+    },
+    {
+        routeName: 'Table2',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
+
+    },
+    {
+        routeName: 'Table3',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
+
+    },
+    {
+        routeName: 'Table4',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
+
+    },
+    {
+        routeName: 'Table5',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
+
+    },
+    {
+        routeName: 'Table6',
+        name: 'Customer Name',
+        phoneNumber: 'Customer Phone Number',
+        email: 'Customer Email',
+        id: 'Customer Id',
+
+    }
+];
