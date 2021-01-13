c11c9d24ebcbe516713286239f9b2c17a314e880
Shannon <shannon.page1218@outlook.com>
2021-01-12 21:37:43 -0500

    Add route pathways

--- back end/app.js
+++ back end/app.js
@@ -7,6 +7,12 @@ var path = require("path");
 var app = express();
 var PORT = 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 // Data
 
 var tables = [
